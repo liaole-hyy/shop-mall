88965e0ac8059e73dd6a75a7da69729a265e5267
liaole-hyy <825888210@qq.com>
2021-05-19 00:03:04 +0800

    加入sentinel限流

--- mall-gateway/mall-api-gateway/src/main/java/com/liaole/mall/api/gateway/config/GateWayConfiguration.java
+++ mall-gateway/mall-api-gateway/src/main/java/com/liaole/mall/api/gateway/config/GateWayConfiguration.java
@@ -1,7 +1,94 @@
 package com.liaole.mall.api.gateway.config;
 
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.SentinelGatewayFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.exception.SentinelGatewayBlockExceptionHandler;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.result.view.ViewResolver;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *  Sentinel 集成配置
  */
+@Configuration
 public class GateWayConfiguration {
+
+    private final List<ViewResolver> viewResolvers;
+    private final ServerCodecConfigurer serverCodecConfigurer;
+
+    public GateWayConfiguration(ObjectProvider<List<ViewResolver>> viewResolversProvider,
+                                ServerCodecConfigurer serverCodecConfigurer) {
+        this.viewResolvers = viewResolversProvider.getIfAvailable(Collections::emptyList);
+        this.serverCodecConfigurer = serverCodecConfigurer;
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SentinelGatewayBlockExceptionHandler sentinelGatewayBlockExceptionHandler() {
+        // Register the block exception handler for Spring Cloud Gateway.
+        return new SentinelGatewayBlockExceptionHandler(viewResolvers, serverCodecConfigurer);
+    }
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public GlobalFilter sentinelGatewayFilter() {
+        return new SentinelGatewayFilter();
+    }
+
+    @PostConstruct
+    public void init(){
+        initCustomizedApis();
+        initCustomizedFlowRules();
+    }
+
+    /**
+     *  定义api组
+     */
+    private void initCustomizedApis() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+        //定义分组
+        ApiDefinition apiDefinition = new ApiDefinition("mall_user_api")
+                .setPredicateItems(new HashSet<ApiPredicateItem>() {
+                    {
+                        add(new ApiPathPredicateItem().setPattern("/mall/user/login"));
+                        add(new ApiPathPredicateItem().setPattern("/mall/user/**")
+                        .setMatchStrategy(SentinelGatewayConstants.URL_MATCH_STRATEGY_PREFIX));
+                    }
+                });
+        definitions.add(apiDefinition);
+        //加入api管理
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+    }
+
+    /**
+     *  定义流控规则
+     */
+    private void initCustomizedFlowRules() {
+        //创建集合存储所有的规则
+        Set<GatewayFlowRule> rules = new HashSet<>();
+
+        //创建新的规则并加入集合中
+        rules.add(new GatewayFlowRule("mall_user_api")
+                    .setCount(1) //请求阈值
+                    .setIntervalSec(10) //统计时间窗口，单位 秒 默认1秒
+                 );
+        GatewayRuleManager.loadRules(rules);
+    }
 }
